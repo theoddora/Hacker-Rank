ad2145eba997d0deb099f2389994d1917d54ca65
ttosheva <ttosheva@degiro.nl>
2017-03-17 16:39:08 +0200

    Solution to Journey to the Moon from HackerRank without Guava.

--- GraphTheory/src/main/java/JourneyToTheMoonWithoutGuava.java
+++ GraphTheory/src/main/java/JourneyToTheMoonWithoutGuava.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-import com.google.common.util.concurrent.AtomicLongMap;
-
-public class JourneyToTheMoon {
+public class JourneyToTheMoonWithoutGuava {
 
     public static void main(String[] args) {
 
@@ -18,23 +14,26 @@ public class JourneyToTheMoon {
             astronauts.add(new ArrayList<Integer>());
         }
 
+        boolean paired[] = new boolean[astronautsNumber];
         for (int i = 0; i < pairs; ++i) {
 
             int cityOne = scan.nextInt(), cityTwo = scan.nextInt();
             astronauts.get(cityOne).add(cityTwo);
             astronauts.get(cityTwo).add(cityOne);
+            paired[cityOne] = true;
+            paired[cityTwo] = true;
         }
         scan.close();
 
         long numberOfPairs = 0;
-        AtomicLongMap<Integer> peopleInGroups = findGroups(astronauts);
+        Map<Integer, Long> peopleInGroups = findGroups(astronauts, paired);
 
         for (int i = 1; i <= peopleInGroups.size(); ++i) {
 
             int indexOfNext = i + 1;
-            long currentNumberOfPairs = peopleInGroups.get(i);
 
             while (indexOfNext <= peopleInGroups.size()) {
+                long currentNumberOfPairs = peopleInGroups.get(i);
                 currentNumberOfPairs *= peopleInGroups.get(indexOfNext);
                 numberOfPairs += currentNumberOfPairs;
                 indexOfNext++;
@@ -44,16 +43,21 @@ public class JourneyToTheMoon {
         System.out.println(numberOfPairs);
     }
 
-    public static AtomicLongMap<Integer> findGroups(List<List<Integer>> astronauts) {
+    public static Map<Integer, Long> findGroups(List<List<Integer>> astronauts, boolean paired[]) {
 
-        AtomicLongMap<Integer> numberOfPeopleInCountry = AtomicLongMap.create();
+        Map<Integer, Long> numberOfPeopleInCountry = new HashMap<>();
 
         boolean visitedAstronaut[] = new boolean[astronauts.size()];
         for (int i = 0, groupId = 0; i < astronauts.size(); ++i) {
 
+            if(!paired[i]) {
+                numberOfPeopleInCountry.put(++groupId, 1L);
+                continue;
+            }
+
             if (!visitedAstronaut[i]) {
                 visitedAstronaut[i] = true;
-                numberOfPeopleInCountry.put(++groupId, 1);
+                numberOfPeopleInCountry.put(++groupId, 1L);
                 findGroups(astronauts, i, groupId, visitedAstronaut, numberOfPeopleInCountry);
             }
         }
@@ -62,8 +66,7 @@ public class JourneyToTheMoon {
     }
 
     private static void findGroups(List<List<Integer>> astronauts, int astronautNumber, int groupId, boolean visitedAstronaut[],
-        AtomicLongMap<Integer> numberOfPeopleInCountry) {
-
+        Map<Integer, Long> numberOfPeopleInCountry) {
 
         for (int i = 0; i < astronauts.get(astronautNumber).size(); ++i) {
 
@@ -71,7 +74,7 @@ public class JourneyToTheMoon {
 
             if (!visitedAstronaut[astronaut]) {
                 visitedAstronaut[astronaut] = true;
-                numberOfPeopleInCountry.getAndIncrement(groupId);
+                numberOfPeopleInCountry.put(groupId, numberOfPeopleInCountry.get(groupId) + 1);
                 findGroups(astronauts, astronaut, groupId, visitedAstronaut, numberOfPeopleInCountry);
             }
 
