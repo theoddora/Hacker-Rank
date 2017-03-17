f9413b1b0170a903c32ca924c2600da0de05f83f
ttosheva <ttosheva@degiro.nl>
2017-03-17 12:52:00 +0200

    Solution to Roads And Libraries from HackerRank

---
+++ GraphTheory/src/RoadsAndLibraries.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RoadsAndLibraries {
+
+    static class CitiesMap {
+
+        private int costOfLibrary;
+        private int costOfRoad;
+        private List<List<Integer>> roads;
+
+        private long totalCost;
+
+        public CitiesMap(int numberOfCities, int costOfLibrary, int costOfRoad) {
+            this.totalCost = 0;
+            this.costOfLibrary = costOfLibrary;
+            this.costOfRoad = costOfRoad;
+            roads = new ArrayList<>();
+            for (int i = 0; i < numberOfCities; ++i) {
+                roads.add(new ArrayList<>());
+            }
+        }
+
+        public List<List<Integer>> getRoads() {
+            return roads;
+        }
+
+        public void buildLibraries(int city, boolean visited[]) {
+
+            List<Integer> citiesToGo = roads.get(city);
+
+            for (Integer aCitiesToGo : citiesToGo) {
+                if (!visited[aCitiesToGo]) {
+
+                    visited[aCitiesToGo] = true;
+                    if (costOfLibrary > costOfRoad) {
+                        totalCost += this.costOfRoad;
+                        buildLibraries(aCitiesToGo, visited);
+                    } else {
+                        totalCost += this.costOfLibrary;
+                        buildLibraries(aCitiesToGo, visited);
+                    }
+                }
+            }
+
+        }
+
+        public long buildLibraries() {
+            boolean visited[] = new boolean[roads.size()];
+            for (int i = 0; i < roads.size(); ++i) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    totalCost += this.costOfLibrary;
+                    buildLibraries(i, visited);
+                }
+            }
+            return totalCost;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int numberOfQueries = scan.nextInt();
+
+        CitiesMap citiesMaps[] = new CitiesMap[numberOfQueries];
+
+        for (int i = 0; i < numberOfQueries; ++i) {
+            int numberOfCities = scan.nextInt();
+            int numberOfRoads = scan.nextInt();
+            int costOfLibrary = scan.nextInt();
+            int costOfRoads = scan.nextInt();
+
+            citiesMaps[i] = new CitiesMap(numberOfCities, costOfLibrary, costOfRoads);
+
+            for (int j = 0; j < numberOfRoads; ++j) {
+                int city_1 = scan.nextInt();
+                int city_2 = scan.nextInt();
+
+                citiesMaps[i].getRoads().get(city_1 - 1).add(city_2 - 1);
+                citiesMaps[i].getRoads().get(city_2 - 1).add(city_1 - 1);
+            }
+        }
+
+        scan.close();
+
+        for (CitiesMap city : citiesMaps) {
+            System.out.println(city.buildLibraries());
+        }
+    }
+
+}
